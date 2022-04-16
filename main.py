6f78381bf6f7aa023b2d793c4348fa4e7a788239
Anton Gundermann <anton.gundermann@gmx.de>
2022-04-16 09:13:02 +0200

    feat:create starting grid

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import pygame
+import random
 
 pygame.init()
 
-WIDTH, HEIGHT = 700, 800
+WIDTH, HEIGHT = 200, 200
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Minesweeper")
 
-FPS = 60
+FPS = 30
 BG_COLOR = "white"
+ROWS, COLS = 10, 10
+CELLS = ROWS * COLS
+CELL_SIZE = 20
+MINES = 5
 
-def draw():
-    pass
+
+def create_field(mines, cells): 
+    field = [0]*cells
+    while field.count(1) < mines:
+        field[random.randint(0, cells-1)] = 1
+    return field
+
+field = create_field(MINES, CELLS)
+print(sum(field), field)
+
+face_down = pygame.transform.scale(pygame.image.load("./assets/facingDown.png"), (CELL_SIZE, CELL_SIZE))
+
+def draw(win):
+    for i in range(CELLS):
+        x = i % ROWS * CELL_SIZE
+        y = i // ROWS * CELL_SIZE
+        win.blit(face_down, (x, y))
+    
 
 
 def run():
@@ -26,8 +47,7 @@ def run():
                 running = False
 
         # Game Logic
-        win.fill(BG_COLOR)
-
+        draw(win)
         pygame.display.flip()       
 
     pygame.quit()
